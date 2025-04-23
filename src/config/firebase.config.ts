4e95b299910618b8e5b2d56a0a8adc4afc8194f4
Trishit Bhowmik <90523629+trishit78@users.noreply.github.com>
2025-04-23 10:35:58 +0530

    added firebase

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,20 @@
+import { initializeApp,getApps,getApp } from "firebase/app"; 
+import {getFirestore,enableNetwork,disableNetwork } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey:import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain:import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId:import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket:import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId:import.meta.env.VITE_FIREBASE_MESSAGING_SENDERID,
+    appId:import.meta.env.VITE_FIREBASE_APP_ID
+}
+
+console.log(import.meta.env.VITE_FIREBASE_API_KEY)
+const app = getApps.length > 0 ? getApp() :initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+await disableNetwork(db); 
+await enableNetwork(db);
+
+export {db};
--- src/layouts/public-layout.tsx
+++ src/layouts/public-layout.tsx
@@ -1,12 +1,13 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
+import AuthHandler from "@/handlers/auth-handler";
 import { Outlet } from "react-router-dom";
 
 const PublicLayout = () => {
   return (
     <div className="w-full">
         {/** handler should store user data */}
-
+        <AuthHandler />
         <Header />
         <Outlet />
         <Footer />
